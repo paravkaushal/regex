2bcf67e6f831ba727eef696abadbe3222ebc1f63
Parav Kaushal <parav.kaushal@indiamart.com>
2021-06-19 11:40:04 +0530

    add validation function

--- validation.js
+++ validation.js
@@ -1,1 +1,22 @@
 // validation script here
+
+const inputs = document.querySelectorAll("input");
+const patterns = {
+  telephone: /^\d{11}$/,
+};
+
+//validation function
+
+function validate(field, regex) {
+  if (regex.test(field.value)) {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
+}
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
